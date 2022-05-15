5aee61210da09f1cfd4e0508c377ed6eddc2c8ac
Didarul Amin <44096577+didarulamin@users.noreply.github.com>
2022-05-15 08:12:54 +0600

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,7 +18,7 @@ let header = $(`
 <div class="collapse navbar-collapse " id="navbarSupportedContent">
   <ul class="navbar-nav ml-auto" id = "navbar-content">
    <li class="nav-item nav-item-hover"><a class="nav-link" href="index.html">Home</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" target="_blank" href="https://drive.google.com/file/d/1olPQ9IyDBNxFMdzFVd1wH9o456tXpRPl/view?usp=sharing">Resume</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" target="_blank" href="https://docs.google.com/document/d/1cfR8_a9xXPlftpgtjiT3QiKxXJmUquVtVDG44MjL1B0/edit?usp=sharing">Resume</a></li>
   <!-- <li class="nav-item nav-item-hover"><a class="nav-link" href="experience.html">Experience</a></li> -->
    <li class="nav-item nav-item-hover"><a class="nav-link" href="projects.html">Projects</a></li>
    <!--<li class="nav-item nav-item-hover"><a class="nav-link" href="research.html">Research</a></li>-->
